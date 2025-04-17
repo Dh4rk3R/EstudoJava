77a24186e720c0423188d56ab2d67887116b5bb3
Kaio Maia <kaio.venicios2014@gmail.com>
2025-04-17 13:19:15 -0300

    Salvando contéudo!

--- src/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-    public static void main(String[]args){
-        System.out.println("Rasensasasas");
-        System.out.println("Raposa");
-        System.out.println("Raposa");
-    }
-}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula01HelloWorld.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um exemplo de comentário tipo <b>javadoc</b>
+ */
+public class Aula01HelloWorld {
+    //Comentário de uma linhas
+    /*
+    Comentário de multiplas linhas
+     */
+    public static void main(String[]args){
+        System.out.println("Rasensasasas");
+        System.out.println("Raposa");
+        System.out.println("Raposa");
+    }
+}
