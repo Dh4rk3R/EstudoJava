9f1b77b7a92caac30f376680198c2fdc4a97bf39
Kaio Maia <kaio.venicios2014@gmail.com>
2025-04-18 00:54:23 -0300

    Tipos primitivos

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,20 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
+        //variaveis primitivas
+        int age = (int)1000000000L;
+        long big_number = 10000;
+        double salary = 10000;
+        float salaryF = 10000;
+        byte ageb = 10;
+        short ages = 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = '\u0042';
 
+        System.out.println("A idade é "+age+" anos");
+        System.out.println("A idade é "+age+" anos");
+        System.out.println("A idade é "+age+" anos");
+        System.out.println("A idade é "+age+" anos");
     }
 }
