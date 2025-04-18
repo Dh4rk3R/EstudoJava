8e46a4b3fbc2b16e41ae73ffbafa26beaa758380
Kaio Maia <kaio.venicios2014@gmail.com>
2025-04-18 01:34:53 -0300

    atividade de Tipos Primitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Kaio";
+        String adress = "Rua 06, Quadra 04, Lote 57";
+        double salary = 12000;
+        String now ="18/04/2025";
+        String relatory = "Eu "+name+", morando no endereço "+adress+", confirmo que recebi o salário de "
+                +salary+", na data "+now;
+
+        System.out.println(relatory);
+
+    }
+}
