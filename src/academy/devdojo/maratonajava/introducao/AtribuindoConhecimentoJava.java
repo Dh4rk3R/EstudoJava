9b87b3a22951e42f45402efff16a3e976390e1b0
Kaio Maia <kaio.venicios2014@gmail.com>
2025-04-30 20:47:13 -0300

    atualização de curso

--- src/academy/devdojo/maratonajava/introducao/AtribuindoConhecimentoJava.java
+++ src/academy/devdojo/maratonajava/introducao/AtribuindoConhecimentoJava.java
@@ -1,18 +1,10 @@
 package academy.devdojo.maratonajava.introducao;
 import java.util.Scanner;
-import java.time.LocalTime;
 
-public class atribuidoConhecimentoJava {
-    public static String gerarPontos(int quant) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < quant; i++) {
-            sb.append(".");
-        }
-        return sb.toString();
-    }
+public class AtribuindoConhecimentoJava {
+
     public static void main(String[] args) throws InterruptedException{
         Scanner scanner = new Scanner(System.in);
-
         int ticket = 0;
         String base = "Processando";
         String base1 = "Validando";
@@ -24,6 +16,23 @@ public class atribuidoConhecimentoJava {
 //        String age = scanner.nextInt();
         int age = scanner.nextInt();
 
+        atribuirPontos(base);
+        Thread.sleep(1500);
+        System.out.println("\nBem Vindo! "+nome+" Estamos validando o valor do Ticket! Um momento!");
+        Thread.sleep(1500);
+
+        atribuirPontos(base1);
+        System.out.println("\nParei aqui...... ");
+    }
+    public static String gerarPontos(int quant) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < quant; i++) {
+            sb.append(".");
+        }
+        return sb.toString();
+    }
+    private static void atribuirPontos(String base) throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             int numPontos = i % 4; // alterna entre 0, 1, 2 e 3
             String pontos = gerarPontos(numPontos);
@@ -31,18 +40,6 @@ public class atribuidoConhecimentoJava {
             System.out.print("\r" + base + pontos); // sobrescreve a linha
             Thread.sleep(500); // espera 0,5 seg
         }
-        Thread.sleep(1500);
-        System.out.println("\nBem Vindo! "+nome+" Estamos validando o valor do Ticket! Um momento!");
-        Thread.sleep(1500);
-
-        for (int i = 0; i < 10; i++) {
-            int numPontos = i % 4; // alterna entre 0, 1, 2 e 3
-            String pontos = gerarPontos(numPontos);
-
-            System.out.print("\r" + base1 + pontos); // sobrescreve a linha
-            Thread.sleep(500); // espera 0,5 seg
-        }
-        System.out.println("\nParei aqui...... ");
     }
 //        if(age)
 
