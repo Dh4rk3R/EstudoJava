d846d8527a9c0e7ef73aa2ceff8bd761327a378f
Kaio Maia <kaio.venicios2014@gmail.com>
2025-05-15 07:04:16 -0300

    atualização de ativi

--- src/academy/devdojo/maratonajava/introducao/Aula06EstruturaDeRepeticaoAti.java
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturaDeRepeticaoAti.java
@@ -2,7 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula06EstruturaDeRepeticaoAti {
     public static void main(String[] args) {
-    int num = 0;
+        //int num = 0;
     /*while(num<1000000){
         System.out.println("Mais um.. Tomalitii " + num);
         num += 2;
@@ -14,14 +14,28 @@ public class Aula06EstruturaDeRepeticaoAti {
     for (num=0; num<=1000000; num +=2){
         System.out.println("Mais um.. Tomalitii " + num);
     }-----------------formas de fazer-------------------
-    for (num=999900; num<=1000000; num++){//ate 1milhao de pá em pá
+    for (num=999900; num<=1000000; num++){       //ate 1milhao de pá em pá
             System.out.println("Mais um.. Tomalitii " + num);
-        }*/
+        }
+
+
+        valor quebrando repetição
     int var = 50;
     for (int i = 0; i <= var; i++) {
             if(i > 25){
                 break;
             }System.out.println(i);
+        }*/
+        double valorTotal = 30000;
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela >= 1000) {
+                System.out.println("Parcela " + parcela + "RS " + valorParcela);
+            }else{
+                break;
+            }
+            System.out.println("Fora do if, mas dentro do for" + parcela);
+
         }
     }
 }
